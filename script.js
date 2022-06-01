d3b96fde92c421c1cb96ba03ca165706c0047c55
OSERHIRDEV <oserhir10@gmail.com>
2022-06-01 14:29:23 +0100

    Functionality for playing the whole game

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 
 
-
-
 window.onload = function () {
     document.getElementById("number-submit").addEventListener("click", playGame);
     document.getElementById("restart-game").addEventListener("click", initGame);
   };
+  
+
+  function playGame() {
+    
+  }
   
