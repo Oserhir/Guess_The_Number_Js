03cdaa0b27cbb879cdf268be9e4f6dd49b9e06a7
OSERHIRDEV <oserhir10@gmail.com>
2022-06-01 14:41:33 +0100

    guess is too high,

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
 window.onload = function () {
   document.getElementById("number-submit").addEventListener("click", playGame);
-  document.getElementById("restart-game").addEventListener("click", initGame);
+  // document.getElementById("restart-game").addEventListener("click", initGame);
 };
 
 let getRandomNumber = () => {
-    return Math.floor(Math.random() * 101);
-  };
-  
-  let correctNumber = getRandomNumber();
+  return Math.floor(Math.random() * 101);
+};
+
+let correctNumber = getRandomNumber();
 
 function playGame() {
   let numberGuess = document.getElementById("number-guess").value;
+  displayResult(numberGuess);
+}
+
+function displayResult(numberGuess) {
+  if (numberGuess > correctNumber) {
+    return "too Hight";
+  } else if (numberGuess < correctNumber) {
+    return "too Low";
+  } else {
+    return "is correct";
+  }
 }
