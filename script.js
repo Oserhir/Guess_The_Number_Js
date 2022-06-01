70455aa5bab0ced1c727d9ad951451ce0bd133c4
OSERHIRDEV <oserhir10@gmail.com>
2022-06-01 14:26:46 +0100

    addEventListener To buttons

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+
+
+window.onload = function () {
+    document.getElementById("number-submit").addEventListener("click", playGame);
+    document.getElementById("restart-game").addEventListener("click", initGame);
+  };
+  
